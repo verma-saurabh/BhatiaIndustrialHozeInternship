0e3deb2b60fdcf24a99957958d82e09160ca5835
Saurabh <saurabh.verma.miet@gmail.com>
2019-12-13 16:47:30 +0530

    First Commit

--- src/main/java/com/example/BhatiaIndustrialHozeInternship/Questions/implementations/QuestionImpl.java
+++ src/main/java/com/example/BhatiaIndustrialHozeInternship/Questions/implementations/QuestionImpl.java
@@ -7,10 +7,6 @@ import com.example.BhatiaIndustrialHozeInternship.Questions.response.SaveQuestio
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import javax.transaction.Transactional;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
